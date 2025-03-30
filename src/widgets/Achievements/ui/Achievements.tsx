c864cc5c9d2fc788165e91f87099272a936f1f14
Фирсов Дмитрий Владимирович <dmitrii.firsov@stm-labs.ru>
2025-03-30 22:40:31 +0200

    feat

--- src/widgets/Achievements/ui/Achievements.tsx
+++ src/widgets/Achievements/ui/Achievements.tsx
@@ -4,7 +4,6 @@ import { translateText } from '@/shared/api';
 
 export const Achievements = async (props: IAchievementsProps) => {
   const { achievements, className } = props;
-  console.log(achievements);
   return (
     <ul className={`ml-4 mt-2 list-disc ${className}`}>
       {achievements.map(async (item) => {
